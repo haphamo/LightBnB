d1384696350e83b9aafd980aaa573d0d2f3bc228
Ha Pham <hapham0392@gmail.com>
2019-10-17 18:20:00 +0000

    updated my allMyReservations to select all coloumns and completed the query for the database to retrieve all reservations for guest

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -1,4 +1,4 @@
-SELECT reservations.id as id, properties.title as title, start_date, avg(rating) as averge_rating
+SELECT reservations.* avg(rating) as averge_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id 
