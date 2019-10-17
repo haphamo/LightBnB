6e2f8b90e84c61c3818a39a85fbee23ed7e262b0
Ha Pham <hapham0392@gmail.com>
2019-10-17 15:33:52 +0000

    installed pg to connect to lightBnB database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');//getting modules to connect to database (bootcampx)
+
+const pool = new Pool({//creating instance of Pool
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightBnB'
+});
 
 /// Users
 
